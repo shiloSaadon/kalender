e6875f684a20ab07181c55f936741ac0c8d36b9c
shiloSaadon <shilosadon@gmail.com>
2026-04-14 18:25:55 +0300

    Fix issues with creating a lot of events fast

--- lib/src/widgets/draggable/day_draggable.dart
+++ lib/src/widgets/draggable/day_draggable.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:kalender/kalender.dart';
 import 'package:kalender/src/models/calendar_events/draggable_event.dart';
-import 'package:kalender/src/widgets/draggable/new_draggable.dart';
 
 /// This widget generates draggable widgets for each visible day.
 /// - These draggable widgets are used to create new events.
 ///
-class DayEventDraggableWidgets<T extends Object?>
-    extends NewDraggableWidget<T> {
+/// Uses StatefulWidget to persist [_localPosition] across rebuilds.
+/// This prevents a race condition where rapid event creation causes
+/// the second event to start at 00:00 due to localPosition being reset
+/// to Offset.zero on widget rebuild.
+class DayEventDraggableWidgets<T extends Object?> extends StatefulWidget {
+  final CalendarController<T> controller;
+  final CalendarCallbacks<T>? callbacks;
   final MultiDayBodyConfiguration bodyConfiguration;
   final DateTimeRange visibleDateTimeRange;
   final TimeOfDayRange timeOfDayRange;
@@ -17,25 +21,44 @@ class DayEventDraggableWidgets<T extends Object?>
 
   const DayEventDraggableWidgets({
     super.key,
-    required super.controller,
+    required this.controller,
     required this.bodyConfiguration,
     required this.visibleDateTimeRange,
     required this.timeOfDayRange,
     required this.pageHeight,
     required this.dayWidth,
     required this.heightPerMinute,
-    required super.callbacks,
+    required this.callbacks,
   });
 
   @override
+  State<DayEventDraggableWidgets<T>> createState() =>
+      _DayEventDraggableWidgetsState<T>();
+}
+
+class _DayEventDraggableWidgetsState<T extends Object?>
+    extends State<DayEventDraggableWidgets<T>> {
+  /// Persisted across rebuilds to prevent race condition when creating events quickly.
+  /// Without this, rebuilds reset localPosition to Offset.zero causing events to start at 00:00.
+  Offset _localPosition = Offset.zero;
+
+  CalendarController<T> get controller => widget.controller;
+  CalendarCallbacks<T>? get callbacks => widget.callbacks;
+  MultiDayBodyConfiguration get bodyConfiguration => widget.bodyConfiguration;
+  DateTimeRange get visibleDateTimeRange => widget.visibleDateTimeRange;
+  TimeOfDayRange get timeOfDayRange => widget.timeOfDayRange;
+  double get dayWidth => widget.dayWidth;
+  double get pageHeight => widget.pageHeight;
+  double get heightPerMinute => widget.heightPerMinute;
+
+  @override
   Widget build(BuildContext context) {
-    var localPosition = Offset.zero;
     final allowCreation = bodyConfiguration.allowEventCreation;
     final createEventTrigger = bodyConfiguration.createEventGesture;
     return Listener(
-      onPointerDown: (event) => localPosition = event.localPosition,
-      onPointerSignal: (event) => localPosition = event.localPosition,
-      onPointerMove: (event) => localPosition = event.localPosition,
+      onPointerDown: (event) => _localPosition = event.localPosition,
+      onPointerSignal: (event) => _localPosition = event.localPosition,
+      onPointerMove: (event) => _localPosition = event.localPosition,
       child: Row(
         children: [
           if (allowCreation)
@@ -43,9 +66,9 @@ class DayEventDraggableWidgets<T extends Object?>
               switch (createEventTrigger) {
                 CreateEventGesture.tap => Draggable(
                     dragAnchorStrategy: pointerDragAnchorStrategy,
-                    onDragStarted: () => createNewEvent(date, localPosition),
-                    onDraggableCanceled: onDragFinished,
-                    onDragEnd: onDragFinished,
+                    onDragStarted: () => _createNewEvent(date, _localPosition),
+                    onDraggableCanceled: _onDragFinished,
+                    onDragEnd: _onDragFinished,
                     data: Create(controllerId: controller.id),
                     feedback: Container(
                       color: Colors.transparent,
@@ -61,9 +84,9 @@ class DayEventDraggableWidgets<T extends Object?>
                 CreateEventGesture.longPress => LongPressDraggable(
                     delay: const Duration(milliseconds: 200),
                     dragAnchorStrategy: pointerDragAnchorStrategy,
-                    onDragStarted: () => createNewEvent(date, localPosition),
-                    onDraggableCanceled: onDragFinished,
-                    onDragEnd: onDragFinished,
+                    onDragStarted: () => _createNewEvent(date, _localPosition),
+                    onDraggableCanceled: _onDragFinished,
+                    onDragEnd: _onDragFinished,
                     data: Create(controllerId: controller.id),
                     feedback: Container(
                       color: Colors.transparent,
@@ -81,7 +104,7 @@ class DayEventDraggableWidgets<T extends Object?>
                       /// It creates a new event at the tapped location.
                       onTapUp: (_) {
                         final newDateTimeRange =
-                            calculateDateTimeRange(date, localPosition);
+                            _calculateDateTimeRange(date, _localPosition);
 
                         final newEvent = CalendarEvent<T>(
                           dateTimeRange: newDateTimeRange,
@@ -106,12 +129,23 @@ class DayEventDraggableWidgets<T extends Object?>
     );
   }
 
+  /// Create the new event and select it where needed.
+  void _createNewEvent(DateTime date, Offset localPosition) {
+    final dateTimeRange = _calculateDateTimeRange(date, localPosition);
+    final newEvent = CalendarEvent<T>(dateTimeRange: dateTimeRange);
+    final event = callbacks?.onEventCreate?.call(newEvent) ?? newEvent;
+    controller.setNewEvent(event);
+    controller.selectEvent(event);
+  }
+
+  /// Deselect the new event.
+  void _onDragFinished([_, __]) => controller.clearNewEvent();
+
   /// Calculate the initial dateTimeRange of a new event.
   ///
   /// [date] is the date the draggable is located at.
   /// [localPosition] is the last known position of the cursor.
-  @override
-  DateTimeRange calculateDateTimeRange(DateTime date, Offset localPosition) {
+  DateTimeRange _calculateDateTimeRange(DateTime date, Offset localPosition) {
     final start = _calculateTimeAndDate(date, localPosition);
     final end = start.add(bodyConfiguration.newEventDuration);
     return DateTimeRange(start: start, end: end);
